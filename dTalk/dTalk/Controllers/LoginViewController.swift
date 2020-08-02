53fd28bacea2f7ac587b04aa53b50fe9b1f61886
NemanjaDucic <69090242+NemanjaDucic@users.noreply.github.com>
2020-08-02 17:03:07 +0200

    Add files via upload

---
+++ dTalk/dTalk/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dTalk
+//
+//  Created by Nikola Ticojevic on 8/1/20.
+//  Copyright © 2020 Nikola Ticojevic. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dTalk/dTalk/Controllers/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  dTalk
+//
+//  Created by Nikola Ticojevic on 8/2/20.
+//  Copyright © 2020 Nikola Ticojevic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class RegisterViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
